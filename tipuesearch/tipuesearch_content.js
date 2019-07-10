0ebdc1b2fbe373f22c83c68a0540cf4c5020492c
Your Name <you@example.com>
2019-07-10 15:22:54 -0400

    demo

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,57 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "Videos", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "Events", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "Calendar", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "About Us", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/about.html"},
+
+      {"title": "Location Map", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/about.html"},
+
+      {"title": "Videos", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/about.html#lastBottom"},
+
+      {"title": "Innovations", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/about.html#lastBottom"},
+
+      {"title": "History", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/about.html#lastBottom"},
+
+     {"title": "Careers", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/careers.html"},
+
+      {"title": "Career", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/careers.html"},
+
+     {"title": "Find A Job", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/find-a-job.html"},
+
+     {"title": "Contact", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/contact.html"},
+
+     {"title": "Finance", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/finance.html"},
+
+     {"title": "Acquisition", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/acquisition.html"},
+
+      {"title": "Testing", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/acquisition.html"},
+
+      {"title": "Nuclear ARms", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/acquisition.html"},
+
+     {"title": "Space and Missiles", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/smsc.html"},
+
+
+      {"title": "Rocket Science", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/smsc.html"},
+
+     {"title": "Register", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/roa.html"},
+
+     {"title": "Career", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/roa.html"},
+
+     {"title": "Careers", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/roa.html"},
+
+     {"title": "AFCS Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "AFCS Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "AFCS Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "AFCS Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"},
+
+     {"title": "AFCS Homepage", "text": "Air Force Civilian Service", "url": "http://new.marcomgroup.com/afcs-homepage.html"}
+]};
